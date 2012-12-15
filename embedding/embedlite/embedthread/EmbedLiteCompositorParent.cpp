c9aae655b90addd6c6e429edc575a9b633f30540
Oleg Romashin <romaxa@gmail.com>
2012-12-14 18:49:22 -0800

    Make Compositor works

--- embedding/embedlite/embedthread/EmbedLiteCompositorParent.cpp
+++ embedding/embedlite/embedthread/EmbedLiteCompositorParent.cpp
@@ -3,6 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#define LOG_COMPONENT "EmbedLiteCompositorParent"
+
 #include "EmbedLiteCompositorParent.h"
 #include "LayerManagerOGL.h"
 #include "mozilla/layers/ShadowLayersParent.h"
