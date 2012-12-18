c2cf116863e7aca5043504dd1e5e7873e68e203b
Oleg Romashin <romaxa@gmail.com>
2012-12-17 21:11:13 -0800

    Added render as URL implementation for testing environment

--- embedding/embedlite/EmbedLiteView.cpp
+++ embedding/embedlite/EmbedLiteView.cpp
@@ -14,6 +14,11 @@
 #include "EmbedLiteViewThreadParent.h"
 #include "EmbedKineticModule.h"
 
+// Image as URL includes
+#include "gfxImageSurface.h"
+#include "mozilla/Base64.h"
+#include "imgIEncoder.h"
+
 namespace mozilla {
 namespace embedlite {
 
@@ -86,6 +91,80 @@ EmbedLiteView::RenderToImage(unsigned char *aData, int imgW, int imgH, int strid
 {
     LOGT("data:%p, sz[%i,%i], stride:%i, depth:%i", aData, imgW, imgH, stride, depth);
     mViewImpl->RenderToImage(aData, imgW, imgH, stride, depth);
+}
+
+char*
+EmbedLiteView::GetImageAsURL(int aWidth, int aHeight)
+{
+    // copy from gfxASurface::WriteAsPNG_internal
+    nsRefPtr<gfxImageSurface> img =
+        new gfxImageSurface(gfxIntSize(aWidth, aHeight), gfxASurface::ImageFormatRGB24);
+    mViewImpl->RenderToImage(img->Data(), img->Width(), img->Height(), img->Stride(), 24);
+    nsCOMPtr<imgIEncoder> encoder =
+        do_CreateInstance("@mozilla.org/image/encoder;2?type=image/png");
+    NS_ENSURE_TRUE(encoder, nullptr);
+    gfxIntSize size = img->GetSize();
+    nsresult rv = encoder->InitFromData(img->Data(),
+                                        size.width * size.height * 4,
+                                        size.width,
+                                        size.height,
+                                        img->Stride(),
+                                        imgIEncoder::INPUT_FORMAT_HOSTARGB,
+                                        NS_LITERAL_STRING(""));
+    if (NS_FAILED(rv))
+        return nullptr;
+    nsCOMPtr<nsIInputStream> imgStream;
+    CallQueryInterface(encoder.get(), getter_AddRefs(imgStream));
+
+    if (!imgStream)
+        return nullptr;
+
+    uint64_t bufSize64;
+    rv = imgStream->Available(&bufSize64);
+    if (NS_FAILED(rv))
+        return nullptr;
+
+    if (bufSize64 > UINT32_MAX - 16)
+        return nullptr;
+
+    uint32_t bufSize = (uint32_t)bufSize64;
+
+    // ...leave a little extra room so we can call read again and make sure we
+    // got everything. 16 bytes for better padding (maybe)
+    bufSize += 16;
+    uint32_t imgSize = 0;
+    char* imgData = (char*)moz_malloc(bufSize);
+    if (!imgData)
+        return nullptr;
+    uint32_t numReadThisTime = 0;
+    while ((rv = imgStream->Read(&imgData[imgSize],
+                                 bufSize - imgSize,
+                                 &numReadThisTime)) == NS_OK && numReadThisTime > 0)
+    {
+        imgSize += numReadThisTime;
+        if (imgSize == bufSize) {
+            // need a bigger buffer, just double
+            bufSize *= 2;
+            char* newImgData = (char*)moz_realloc(imgData, bufSize);
+            if (!newImgData) {
+                moz_free(imgData);
+                return nullptr;
+            }
+            imgData = newImgData;
+        }
+    }
+
+    // base 64, result will be NULL terminated
+    nsCString encodedImg;
+    rv = Base64Encode(Substring(imgData, imgSize), encodedImg);
+    moz_free(imgData);
+    if (NS_FAILED(rv)) // not sure why this would fail
+        return nullptr;
+
+    nsCString string("data:image/png;base64,");
+    string.Append(encodedImg);
+
+    return ToNewCString(string);
 }
 
 bool
