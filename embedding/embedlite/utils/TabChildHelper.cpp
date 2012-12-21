e2ae2f4e1a1d87cc95e561873944b4ad88e21cda
Oleg Romashin <romaxa@gmail.com>
2012-12-20 19:30:23 -0800

    Switch back to old nsViewportInfo

--- embedding/embedlite/utils/TabChildHelper.cpp
+++ embedding/embedlite/utils/TabChildHelper.cpp
@@ -126,156 +126,156 @@ TabChildHelper::SetCSSViewport(float aWidth, float aHeight)
 void
 TabChildHelper::HandlePossibleViewportChange()
 {
-    nsCOMPtr<nsIDOMDocument> domDoc;
-    mView->mWebNavigation->GetDocument(getter_AddRefs(domDoc));
-    nsCOMPtr<nsIDocument> document(do_QueryInterface(domDoc));
 
-    nsCOMPtr<nsPIDOMWindow> window = do_GetInterface(mView->mWebNavigation);
-    nsCOMPtr<nsIDOMWindowUtils> utils = do_GetInterface(window);
+  nsCOMPtr<nsIDOMDocument> domDoc;
+  mView->mWebNavigation->GetDocument(getter_AddRefs(domDoc));
+  nsCOMPtr<nsIDocument> document(do_QueryInterface(domDoc));
 
-#if 0
-    ViewportInfo viewportInfo =
-        nsContentUtils::GetViewportInfo(document, mInnerSize.width, mInnerSize.height);
-    mView->SendUpdateZoomConstraints(viewportInfo.allowZoom,
-                                     viewportInfo.minZoom,
-                                     viewportInfo.maxZoom);
+  nsCOMPtr<nsPIDOMWindow> window = do_GetInterface(mView->mWebNavigation);
+  nsCOMPtr<nsIDOMWindowUtils> utils = do_GetInterface(window);
 
-    float screenW = mInnerSize.width;
-    float screenH = mInnerSize.height;
-    float viewportW = viewportInfo.width;
-    float viewportH = viewportInfo.height;
+  nsViewportInfo viewportInfo =
+    nsContentUtils::GetViewportInfo(document, mInnerSize.width, mInnerSize.height);
+  mView->SendUpdateZoomConstraints(viewportInfo.IsZoomAllowed(),
+                                   viewportInfo.GetMinZoom(),
+                                   viewportInfo.GetMaxZoom());
 
-    // We're not being displayed in any way; don't bother doing anything because
-    // that will just confuse future adjustments.
-    if (!screenW || !screenH) {
-        return;
+  float screenW = mInnerSize.width;
+  float screenH = mInnerSize.height;
+  float viewportW = viewportInfo.GetWidth();
+  float viewportH = viewportInfo.GetHeight();
+
+  // We're not being displayed in any way; don't bother doing anything because
+  // that will just confuse future adjustments.
+  if (!screenW || !screenH) {
+    return;
+  }
+
+  // Make sure the viewport height is not shorter than the window when the page
+  // is zoomed out to show its full width. Note that before we set the viewport
+  // width, the "full width" of the page isn't properly defined, so that's why
+  // we have to call SetCSSViewport twice - once to set the width, and the
+  // second time to figure out the height based on the layout at that width.
+  float oldBrowserWidth = mOldViewportWidth;
+  mLastMetrics.mViewport.width = viewportW;
+  mLastMetrics.mViewport.height = viewportH;
+  if (!oldBrowserWidth) {
+    oldBrowserWidth = kDefaultViewportSize.width;
+  }
+  SetCSSViewport(viewportW, viewportH);
+
+  // If this page has not been painted yet, then this must be getting run
+  // because a meta-viewport element was added (via the DOMMetaAdded handler).
+  // in this case, we should not do anything that forces a reflow (see bug
+  // 759678) such as requesting the page size or sending a viewport update. this
+  // code will get run again in the before-first-paint handler and that point we
+  // will run though all of it. the reason we even bother executing up to this
+  // point on the DOMMetaAdded handler is so that scripts that use
+  // window.innerWidth before they are painted have a correct value (bug
+  // 771575).
+  if (!mContentDocumentIsDisplayed) {
+    return;
+  }
+
+  float minScale = 1.0f;
+
+  nsCOMPtr<nsIDOMElement> htmlDOMElement = do_QueryInterface(document->GetHtmlElement());
+  nsCOMPtr<nsIDOMElement> bodyDOMElement = do_QueryInterface(document->GetBodyElement());
+
+  int32_t htmlWidth = 0, htmlHeight = 0;
+  if (htmlDOMElement) {
+    htmlDOMElement->GetScrollWidth(&htmlWidth);
+    htmlDOMElement->GetScrollHeight(&htmlHeight);
+  }
+  int32_t bodyWidth = 0, bodyHeight = 0;
+  if (bodyDOMElement) {
+    bodyDOMElement->GetScrollWidth(&bodyWidth);
+    bodyDOMElement->GetScrollHeight(&bodyHeight);
+  }
+
+  float pageWidth, pageHeight;
+  if (htmlDOMElement || bodyDOMElement) {
+    pageWidth = NS_MAX(htmlWidth, bodyWidth);
+    pageHeight = NS_MAX(htmlHeight, bodyHeight);
+  } else {
+    // For non-HTML content (e.g. SVG), just assume page size == viewport size.
+    pageWidth = viewportW;
+    pageHeight = viewportH;
+  }
+  NS_ENSURE_TRUE_VOID(pageWidth); // (return early rather than divide by 0)
+
+  minScale = mInnerSize.width / pageWidth;
+  minScale = clamped((double)minScale, viewportInfo.GetMinZoom(),
+                     viewportInfo.GetMaxZoom());
+  NS_ENSURE_TRUE_VOID(minScale); // (return early rather than divide by 0)
+
+  viewportH = NS_MAX(viewportH, screenH / minScale);
+  SetCSSViewport(viewportW, viewportH);
+
+  // This change to the zoom accounts for all types of changes I can conceive:
+  // 1. screen size changes, CSS viewport does not (pages with no meta viewport
+  //    or a fixed size viewport)
+  // 2. screen size changes, CSS viewport also does (pages with a device-width
+  //    viewport)
+  // 3. screen size remains constant, but CSS viewport changes (meta viewport
+  //    tag is added or removed)
+  // 4. neither screen size nor CSS viewport changes
+  //
+  // In all of these cases, we maintain how much actual content is visible
+  // within the screen width. Note that "actual content" may be different with
+  // respect to CSS pixels because of the CSS viewport size changing.
+  int32_t oldScreenWidth = mLastMetrics.mCompositionBounds.width;
+  if (!oldScreenWidth) {
+    oldScreenWidth = mInnerSize.width;
+  }
+
+  FrameMetrics metrics(mLastMetrics);
+  metrics.mViewport = gfx::Rect(0.0f, 0.0f, viewportW, viewportH);
+  metrics.mScrollableRect = gfx::Rect(0.0f, 0.0f, pageWidth, pageHeight);
+  metrics.mCompositionBounds = nsIntRect(0, 0, mInnerSize.width, mInnerSize.height);
+
+  // Changing the zoom when we're not doing a first paint will get ignored
+  // by AsyncPanZoomController and causes a blurry flash.
+  bool isFirstPaint;
+  nsresult rv = utils->GetIsFirstPaint(&isFirstPaint);
+  MOZ_ASSERT(NS_SUCCEEDED(rv));
+  if (NS_FAILED(rv) || isFirstPaint) {
+    gfxSize intrinsicScale =
+        AsyncPanZoomController::CalculateIntrinsicScale(metrics);
+    // FIXME/bug 799585(?): GetViewportInfo() returns a defaultZoom of
+    // 0.0 to mean "did not calculate a zoom".  In that case, we default
+    // it to the intrinsic scale.
+    if (viewportInfo.GetDefaultZoom() < 0.01f) {
+      viewportInfo.SetDefaultZoom(intrinsicScale.width);
     }
 
-    // Make sure the viewport height is not shorter than the window when the page
-    // is zoomed out to show its full width. Note that before we set the viewport
-    // width, the "full width" of the page isn't properly defined, so that's why
-    // we have to call SetCSSViewport twice - once to set the width, and the
-    // second time to figure out the height based on the layout at that width.
-    float oldBrowserWidth = mOldViewportWidth;
-    mLastMetrics.mViewport.width = viewportInfo.width;
-    mLastMetrics.mViewport.height = viewportInfo.height;
-    if (!oldBrowserWidth) {
-        oldBrowserWidth = kDefaultViewportSize.width;
-    }
-    SetCSSViewport(viewportW, viewportH);
+    double defaultZoom = viewportInfo.GetDefaultZoom();
+    MOZ_ASSERT(viewportInfo.GetMinZoom() <= defaultZoom &&
+               defaultZoom <= viewportInfo.GetMaxZoom());
+    // GetViewportInfo() returns a resolution-dependent scale factor.
+    // Convert that to a resolution-indepedent zoom.
+    metrics.mZoom = gfxSize(defaultZoom / intrinsicScale.width,
+                            defaultZoom / intrinsicScale.height);
+  }
 
-    // If this page has not been painted yet, then this must be getting run
-    // because a meta-viewport element was added (via the DOMMetaAdded handler).
-    // in this case, we should not do anything that forces a reflow (see bug
-    // 759678) such as requesting the page size or sending a viewport update. this
-    // code will get run again in the before-first-paint handler and that point we
-    // will run though all of it. the reason we even bother executing up to this
-    // point on the DOMMetaAdded handler is so that scripts that use
-    // window.innerWidth before they are painted have a correct value (bug
-    // 771575).
-    if (!mContentDocumentIsDisplayed) {
-        return;
-    }
+  metrics.mDisplayPort = AsyncPanZoomController::CalculatePendingDisplayPort(
+    // The page must have been refreshed in some way such as a new document or
+    // new CSS viewport, so we know that there's no velocity, acceleration, and
+    // we have no idea how long painting will take.
+    metrics, gfx::Point(0.0f, 0.0f), gfx::Point(0.0f, 0.0f), 0.0);
+  gfxSize resolution = AsyncPanZoomController::CalculateResolution(metrics);
+  // XXX is this actually hysteresis?  This calculation is not well
+  // understood.  It's taken from the previous JS implementation.
+  gfxFloat hysteresis/*?*/ =
+    gfxFloat(oldBrowserWidth) / gfxFloat(oldScreenWidth);
+  resolution.width *= hysteresis;
+  resolution.height *= hysteresis;
+  metrics.mResolution = resolution;
+  utils->SetResolution(metrics.mResolution.width, metrics.mResolution.height);
 
-    float minScale = 1.0f;
-
-    nsCOMPtr<nsIDOMElement> htmlDOMElement = do_QueryInterface(document->GetHtmlElement());
-    nsCOMPtr<nsIDOMElement> bodyDOMElement = do_QueryInterface(document->GetBodyElement());
-
-    int32_t htmlWidth = 0, htmlHeight = 0;
-    if (htmlDOMElement) {
-        htmlDOMElement->GetScrollWidth(&htmlWidth);
-        htmlDOMElement->GetScrollHeight(&htmlHeight);
-    }
-    int32_t bodyWidth = 0, bodyHeight = 0;
-    if (bodyDOMElement) {
-        bodyDOMElement->GetScrollWidth(&bodyWidth);
-        bodyDOMElement->GetScrollHeight(&bodyHeight);
-    }
-
-    float pageWidth, pageHeight;
-    if (htmlDOMElement || bodyDOMElement) {
-        pageWidth = NS_MAX(htmlWidth, bodyWidth);
-        pageHeight = NS_MAX(htmlHeight, bodyHeight);
-    } else {
-        // For non-HTML content (e.g. SVG), just assume page size == viewport size.
-        pageWidth = viewportW;
-        pageHeight = viewportH;
-    }
-    NS_ENSURE_TRUE_VOID(pageWidth); // (return early rather than divide by 0)
-
-    minScale = mInnerSize.width / pageWidth;
-    minScale = clamped((double)minScale, viewportInfo.minZoom, viewportInfo.maxZoom);
-    NS_ENSURE_TRUE_VOID(minScale); // (return early rather than divide by 0)
-
-    viewportH = NS_MAX(viewportH, screenH / minScale);
-    SetCSSViewport(viewportW, viewportH);
-
-    // This change to the zoom accounts for all types of changes I can conceive:
-    // 1. screen size changes, CSS viewport does not (pages with no meta viewport
-    //    or a fixed size viewport)
-    // 2. screen size changes, CSS viewport also does (pages with a device-width
-    //    viewport)
-    // 3. screen size remains constant, but CSS viewport changes (meta viewport
-    //    tag is added or removed)
-    // 4. neither screen size nor CSS viewport changes
-    //
-    // In all of these cases, we maintain how much actual content is visible
-    // within the screen width. Note that "actual content" may be different with
-    // respect to CSS pixels because of the CSS viewport size changing.
-    int32_t oldScreenWidth = mLastMetrics.mCompositionBounds.width;
-    if (!oldScreenWidth) {
-        oldScreenWidth = mInnerSize.width;
-    }
-
-    FrameMetrics metrics(mLastMetrics);
-    metrics.mViewport = gfx::Rect(0.0f, 0.0f, viewportW, viewportH);
-    metrics.mScrollableRect = gfx::Rect(0.0f, 0.0f, pageWidth, pageHeight);
-    metrics.mCompositionBounds = nsIntRect(0, 0, mInnerSize.width, mInnerSize.height);
-
-    // Changing the zoom when we're not doing a first paint will get ignored
-    // by AsyncPanZoomController and causes a blurry flash.
-    bool isFirstPaint;
-    nsresult rv = utils->GetIsFirstPaint(&isFirstPaint);
-    MOZ_ASSERT(NS_SUCCEEDED(rv));
-    if (NS_FAILED(rv) || isFirstPaint) {
-        gfxSize intrinsicScale =
-            AsyncPanZoomController::CalculateIntrinsicScale(metrics);
-        // FIXME/bug 799585(?): GetViewportInfo() returns a defaultZoom of
-        // 0.0 to mean "did not calculate a zoom".  In that case, we default
-        // it to the intrinsic scale.
-        if (viewportInfo.defaultZoom < 0.01f) {
-            viewportInfo.defaultZoom = intrinsicScale.width;
-        }
-        MOZ_ASSERT(viewportInfo.minZoom <= viewportInfo.defaultZoom &&
-                   viewportInfo.defaultZoom <= viewportInfo.maxZoom);
-        // GetViewportInfo() returns a resolution-dependent scale factor.
-        // Convert that to a resolution-indepedent zoom.
-        metrics.mZoom = gfxSize(viewportInfo.defaultZoom / intrinsicScale.width,
-                                viewportInfo.defaultZoom / intrinsicScale.height);
-    }
-
-    metrics.mDisplayPort = AsyncPanZoomController::CalculatePendingDisplayPort(
-        // The page must have been refreshed in some way such as a new document or
-        // new CSS viewport, so we know that there's no velocity, acceleration, and
-        // we have no idea how long painting will take.
-        metrics, gfx::Point(0.0f, 0.0f), gfx::Point(0.0f, 0.0f), 0.0);
-    gfxSize resolution = AsyncPanZoomController::CalculateResolution(metrics);
-    // XXX is this actually hysteresis?  This calculation is not well
-    // understood.  It's taken from the previous JS implementation.
-    gfxFloat hysteresis/*?*/ =
-        gfxFloat(oldBrowserWidth) / gfxFloat(oldScreenWidth);
-    resolution.width *= hysteresis;
-    resolution.height *= hysteresis;
-    metrics.mResolution = resolution;
-    utils->SetResolution(metrics.mResolution.width, metrics.mResolution.height);
-
-    // Force a repaint with these metrics. This, among other things, sets the
-    // displayport, so we start with async painting.
-    RecvUpdateFrame(metrics);
-#else
-#warning "Fix nsViewPort include"
-#endif
+  // Force a repaint with these metrics. This, among other things, sets the
+  // displayport, so we start with async painting.
+  RecvUpdateFrame(metrics);
 }
 
 static void
