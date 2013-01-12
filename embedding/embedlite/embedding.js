7477894065d8ad020b841b00168d126ec14e5947
Oleg Romashin <romaxa@gmail.com>
2013-01-11 18:48:56 -0800

    Move default prefs into .js pref file

--- embedding/embedlite/embedding.js
+++ embedding/embedlite/embedding.js
@@ -1,2 +1,65 @@
 pref("dom.w3c_touch_events.enabled", 1);
-
+pref("plugin.disable", true);
+pref("dom.ipc.plugins.enabled", true);
+pref("plugins.force.wmode", "opaque");
+pref("browser.xul.error_pages.enabled", true);
+pref("gfx.color_management.mode", 0);
+//pref("general.useragent.override", "Mozilla/5.0 (Maemo; Linux armv7l; rv:14.0) Gecko/17.0 Firefox/17.0");
+pref("general.useragent.override", "Mozilla/5.0 (Android; Mobile; rv:12.0) Gecko/17.0 Firefox/17.0");
+pref("nglayout.debug.paint_flashing", false);
+pref("nglayout.debug.widget_update_flashing", false);
+// Perf trick, speedup motion handlers
+pref("layout.reflow.synthMouseMove", false);
+// Disable Native themeing because it is usually broken
+pref("mozilla.widget.disable-native-theme", true);
+// Override some named colors to avoid inverse OS themes
+pref("ui.-moz-dialog", "#efebe7");
+pref("ui.-moz-dialogtext", "#101010");
+pref("ui.-moz-field", "#fff");
+pref("ui.-moz-fieldtext", "#1a1a1a");
+pref("ui.-moz-buttonhoverface", "#f3f0ed");
+pref("ui.-moz-buttonhovertext", "#101010");
+pref("ui.-moz-combobox", "#fff");
+pref("ui.-moz-comboboxtext", "#101010");
+pref("ui.buttonface", "#ece7e2");
+pref("ui.buttonhighlight", "#fff");
+pref("ui.buttonshadow", "#aea194");
+pref("ui.buttontext", "#101010");
+pref("ui.captiontext", "#101010");
+pref("ui.graytext", "#b1a598");
+pref("ui.highlight", "#fad184");
+pref("ui.highlighttext", "#1a1a1a");
+pref("ui.infobackground", "#f5f5b5");
+pref("ui.infotext", "#000");
+pref("ui.menu", "#f7f5f3");
+pref("ui.menutext", "#101010");
+pref("ui.threeddarkshadow", "#000");
+pref("ui.threedface", "#ece7e2");
+pref("ui.threedhighlight", "#fff");
+pref("ui.threedlightshadow", "#ece7e2");
+pref("ui.threedshadow", "#aea194");
+pref("ui.window", "#efebe7");
+pref("ui.windowtext", "#101010");
+pref("ui.windowframe", "#efebe7");
+// Enable webgl by force
+pref("webgl.force-enabled", true);
+// Setup dumping enabled for development
+pref("browser.dom.window.dump.enabled", true);
+pref("layers.acceleration.draw-fps", false);
+// SetPref(QString("browser.xul.error_pages.enabled", false);
+// Bug 706179 async animation temporary prefs
+pref("layers.offmainthreadcomposition.animate-opacity", true);
+pref("layers.offmainthreadcomposition.animate-transform", true);
+pref("layers.async-video.enabled", true);
+pref("font.size.inflation.disabledInMasterProcess", true);
+pref("gfx.azpc.pan_repaint_interval", 5050);
+pref("gfx.azpc.y_skate_size_multiplier", "4.5f");
+pref("gfx.azpc.y_stationary_size_multiplier", "4.5f");
+pref("gfx.axis.max_event_acceleration", "12.0f");
+pref("gfx.axis.fling_friction", "0.00245f");
+pref("gfx.azpc.min_skate_speed", "10.0f");
+pref("embedlite.handle_viewport", false);
+pref("ui.dragThresholdX", 25);
+pref("ui.dragThresholdY", 25);
+pref("layout.build_layers_for_scrollable_views", true);
+pref("embedlite.dispatch_mouse_events", false); // Will dispatch mouse events if page using them
