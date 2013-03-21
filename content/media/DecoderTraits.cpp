b2dd4c63c1cea6d945081b4f609e45716f7cedf7
Tatiana Meshkova <tanya.meshkova@gmail.com>
2013-03-20 19:25:46 -0700

    Revert "Bug 829653 - fix content/media build with --enable-gstreamer"
    This reverts commit 5156481c4a2a592faabf14626920964ddf6d2e3b.

--- content/media/DecoderTraits.cpp
+++ content/media/DecoderTraits.cpp
@@ -183,12 +183,6 @@ static const char* const gH264Types[4] = {
 };
 
 static bool
-IsH264Type(const nsACString& aType)
-{
-  return CodecListContains(gH264Types, aType);
-}
-
-static bool
 IsGStreamerSupportedType(const nsACString& aMimeType)
 {
   if (!MediaDecoder::IsGStreamerEnabled())
@@ -206,6 +200,12 @@ IsGStreamerSupportedType(const nsACString& aMimeType)
     return true;
 #endif
   return false;
+}
+
+static bool
+IsH264Type(const nsACString& aType)
+{
+  return CodecListContains(gH264Types, aType);
 }
 #endif
 
