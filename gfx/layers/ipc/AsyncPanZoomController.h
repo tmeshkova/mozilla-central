97862a72d28381fe026fecb58c9c3ac8700d3aa9
Dmitry Rozhkov <dmitry.rozhkov@jollamobile.com>
2013-04-15 12:14:54 +0300

    Revert "Merge pull request #17 from rojkov/embedlite"
    This reverts commit 9760efc024ef86e2ebd3580cb84b52d6a3fc7815, reversing
    changes made to b3ad130b13db2fb5d10fbb00c0e714471d56cf34.

--- gfx/layers/ipc/AsyncPanZoomController.h
+++ gfx/layers/ipc/AsyncPanZoomController.h
@@ -507,10 +507,6 @@ private:
    */
   void SetState(PanZoomState aState);
 
-/** Locks scrolling if velocity vector is close to an axis.
- */
-  void LockScroll();
-
   nsRefPtr<CompositorParent> mCompositorParent;
   TaskThrottler mPaintThrottler;
   nsRefPtr<GeckoContentController> mGeckoContentController;
