164370064bd2df5a674096f47b74268c67ad1f89
Tatiana Meshkova <tanya.meshkova@gmail.com>
2013-10-17 08:15:10 -0700

    Disable loading of 2 instances HW accelerated gstreamer pipeline

--- content/media/gstreamer/GStreamerReader.cpp
+++ content/media/gstreamer/GStreamerReader.cpp
@@ -55,6 +55,8 @@ typedef enum {
   GST_PLAY_FLAG_SOFT_COLORBALANCE = (1 << 10)
 } PlayFlags;
 
+static bool sDroidEGLSinkInUse = false;
+
 GStreamerReader::GStreamerReader(AbstractMediaDecoder* aDecoder)
   : MediaDecoderReader(aDecoder),
   mPlayBin(nullptr),
@@ -105,6 +107,7 @@ GStreamerReader::~GStreamerReader()
     mPlayingStartedOnce = false;
     mPlayBin = nullptr;
     mPlaySink = nullptr;
+    sDroidEGLSinkInUse = false;
     mVideoSink = nullptr;
     mVideoAppSink = nullptr;
     mAudioSink = nullptr;
@@ -129,10 +132,16 @@ nsresult GStreamerReader::Init(MediaDecoderReader* aCloneDonor)
     return NS_ERROR_FAILURE;
   }
 #ifdef HAS_NEMO_INTERFACE
+  if (sDroidEGLSinkInUse && !Preferences::GetBool("gstreamer.no_hw_decoder_limit", false))
+  {
+    return NS_ERROR_FAILURE;
+  }
   mPlaySink = gst_element_factory_make("droideglsink", nullptr);
   if (!mPlaySink) {
     LOG(PR_LOG_DEBUG, ("could not create egl sink: %p", mPlaySink));
+    return NS_ERROR_FAILURE;
   }
+  sDroidEGLSinkInUse = true;
 #endif
   g_object_set(mPlayBin, "buffer-size", 0, nullptr);
   mBus = gst_pipeline_get_bus(GST_PIPELINE(mPlayBin));
